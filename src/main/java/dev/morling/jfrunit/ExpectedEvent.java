76cd94a24650c53b86766298181c83788a436454
zhanghaoxin-at-826767166263 <COk6iTbDdOqj/yq53u9n9vPTy+3pmZNcm4mKOhImudI=>
2021-01-06 02:15:22 +0000

    add all event attribute types

--- src/main/java/dev/morling/jfrunit/ExpectedEvent.java
+++ src/main/java/dev/morling/jfrunit/ExpectedEvent.java
@@ -16,9 +16,11 @@
 package dev.morling.jfrunit;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 import jdk.jfr.consumer.RecordedEvent;
@@ -73,10 +75,37 @@ public class ExpectedEvent implements Predicate<RecordedEvent> {
         // TODO all event attribute types
 
         if (value instanceof Duration) {
-            return recordedEvent.getDuration(name).equals(value);
+            return Objects.equals(recordedEvent.getDuration(name), value);
         }
         else if (value instanceof String) {
-            return recordedEvent.getString(name).equals(value);
+            return Objects.equals(recordedEvent.getString(name), value);
+        }
+        else if (value instanceof Boolean) {
+            return Objects.equals(recordedEvent.getBoolean(name), value);
+        }
+        else if (value instanceof Boolean) {
+            return Objects.equals(recordedEvent.getByte(name), value);
+        }
+        else if (value instanceof Character) {
+            return Objects.equals(recordedEvent.getChar(name), value);
+        }
+        else if (value instanceof Double) {
+            return Objects.equals(recordedEvent.getDouble(name), value);
+        }
+        else if (value instanceof Float) {
+            return Objects.equals(recordedEvent.getFloat(name), value);
+        }
+        else if (value instanceof Integer) {
+            return Objects.equals(recordedEvent.getInt(name), value);
+        }
+        else if (value instanceof Long) {
+            return Objects.equals(recordedEvent.getLong(name), value);
+        }
+        else if (value instanceof Short) {
+            return Objects.equals(recordedEvent.getShort(name), value);
+        }
+        else if (value instanceof Instant) {
+            return Objects.equals(recordedEvent.getInstant(name), value);
         }
         else {
             throw new IllegalArgumentException(String.format("Unsupported property type: %s, %s", name, value));
