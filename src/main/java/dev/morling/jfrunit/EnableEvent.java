358fbe7745d31c2d3bea7c51b48446558f9e7e37
Tushar Badgu <tushar.badgu@gmail.com>
2021-01-18 11:43:51 +0530

    Fix issue where stacktraces were been captured even when explicitly setup to be excluded
    Signed-off-by: Tushar Badgu <tushar.badgu@gmail.com>

--- src/main/java/dev/morling/jfrunit/EnableEvent.java
+++ src/main/java/dev/morling/jfrunit/EnableEvent.java
@@ -1,23 +1,24 @@
 /**
- *  Copyright 2020 The JfrUnit authors
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright 2020 The JfrUnit authors
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package dev.morling.jfrunit;
 
 import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+
 /**
  * Enables a specific JFR event type, e.g. "jdk.GarbageCollection" for a recording.
  *
@@ -32,18 +33,16 @@ public @interface EnableEvent {
      */
     String value() default "";
 
-//    String name() default "";
-
-    StacktracePolicy stackTrace() default StacktracePolicy.DEFAULT;
+    StacktracePolicy stackTrace() default StacktracePolicy.INCLUDED;
 
     int threshold() default -1;
 
-    public enum StacktracePolicy {
-        DEFAULT, INCLUDED, EXCLUDED;
+    enum StacktracePolicy {
+        INCLUDED, EXCLUDED
     }
 
     @Retention(RetentionPolicy.RUNTIME)
-    public @interface List {
+    @interface List {
         EnableEvent[] value();
     }
 }
--- src/test/java/dev/morling/jfrunit/JfrUnitTest.java
+++ src/test/java/dev/morling/jfrunit/JfrUnitTest.java
@@ -1,27 +1,27 @@
 /**
- *  Copyright 2020 The JfrUnit authors
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Copyright 2020 The JfrUnit authors
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package dev.morling.jfrunit;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
 
 import static dev.morling.jfrunit.ExpectedEvent.event;
 import static dev.morling.jfrunit.JfrEventsAssert.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
-
-import java.time.Duration;
-
-import org.junit.jupiter.api.Test;
 
 @JfrEventTest
 public class JfrUnitTest {
@@ -69,5 +69,35 @@ public class JfrUnitTest {
                 .sum();
 
         assertThat(allocated).isGreaterThan(0);
+    }
+
+    @Test
+    @EnableEvent("jdk.ThreadSleep")
+    public void shouldHaveStackTraceCapturedWithNoStackTracePolicyDefined() throws Exception {
+        Thread.sleep(1000);
+
+        jfrEvents.awaitEvents();
+
+        assertThat(jfrEvents).contains(event("jdk.ThreadSleep").has("stackTrace"));
+    }
+
+    @Test
+    @EnableEvent(value = "jdk.ThreadSleep", stackTrace = EnableEvent.StacktracePolicy.INCLUDED)
+    public void shouldHaveStackTraceCapturedWithStackTracePolicyIncluded() throws Exception {
+        Thread.sleep(1000);
+
+        jfrEvents.awaitEvents();
+
+        assertThat(jfrEvents).contains(event("jdk.ThreadSleep").has("stackTrace"));
+    }
+
+    @Test
+    @EnableEvent(value = "jdk.ThreadSleep", stackTrace = EnableEvent.StacktracePolicy.EXCLUDED)
+    public void shouldNotHaveStackTraceCapturedWithStackTracePolicyExcluded() throws Exception {
+        Thread.sleep(1000);
+
+        jfrEvents.awaitEvents();
+
+        assertThat(jfrEvents).contains(event("jdk.ThreadSleep").hasNot("stackTrace"));
     }
 }
