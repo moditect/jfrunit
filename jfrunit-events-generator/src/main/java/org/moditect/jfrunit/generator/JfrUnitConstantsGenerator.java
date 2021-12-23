380426ee37f6851ef15d4499b15d3aa3d20aed9a
Federico Mariani <federico.mariani.1990@gmail.com>
2021-12-23 13:03:49 +0100

    🐛 #115 Fix static variable name
    Add unit test
    
    🐛 #115 Fix static variable name

--- jfrunit-events-generator/src/main/java/org/moditect/jfrunit/generator/JfrUnitConstantsGenerator.java
+++ jfrunit-events-generator/src/main/java/org/moditect/jfrunit/generator/JfrUnitConstantsGenerator.java
@@ -1,3 +1,20 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright 2020 - 2021 The JfrUnit authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.moditect.jfrunit.generator;
 
 import java.io.BufferedInputStream;
@@ -151,5 +168,44 @@ public class JfrUnitConstantsGenerator {
 
     static {
         MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public static String pascalCaseToSnakeCase(String pascalName) {
+        if (pascalName == null) {
+            return null;
+        }
+
+        StringBuilder result = new StringBuilder();
+        char[] charArray = pascalName.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            Character c = pascalName.charAt(i);
+
+            int nextCharPos = i + 1;
+
+            if (i > 0 && nextCharPos < charArray.length) {
+                char previous = pascalName.charAt(i - 1);
+                char next = pascalName.charAt(nextCharPos);
+
+                if ((Character.isLowerCase(c) && Character.isUpperCase(next))
+                        || (!Character.isLetter(c) && Character.isLetter(next))) {
+                    result.append(c.toString().toUpperCase());
+                    result.append("_");
+                }
+                else if (Character.isUpperCase(previous)
+                        && Character.isUpperCase(c)
+                        && Character.isLowerCase(next)) {
+                    result.append("_");
+                    result.append(c.toString().toUpperCase());
+                }
+                else {
+                    result.append(c.toString().toUpperCase());
+                }
+            }
+            else {
+                result.append(c.toString().toUpperCase());
+            }
+        }
+
+        return result.toString();
     }
 }
